b0cf1131def322f9d65857baf55967920d6e7d52
Rahul Tak <codebase.rahul@gmail.com>
2026-02-07 22:39:18 +0530

    ternary operator

---
+++ Conditional Statement/ternaryOperator.java
@@ -0,0 +1,8 @@
+public class ternaryOperator {
+    public static void main(String[] args) {
+        int a = 100;
+
+        String type = (a % 2 == 0) ? "number is even" : "number is odd";
+        System.out.println(type);
+    }
+}
