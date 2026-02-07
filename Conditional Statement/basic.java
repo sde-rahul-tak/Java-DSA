5f4a9fceb9c19ee01de5f2bb3d37e8bfc1020f11
Rahul Tak <codebase.rahul@gmail.com>
2026-02-07 22:04:46 +0530

    basic if else

---
+++ Conditional Statement/basic.java
@@ -0,0 +1,13 @@
+public class basic {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 15;
+
+        if(a>b){
+            System.out.println("a is graterthan b");
+        }
+        else{
+            System.out.println("b is graterthan");
+        }
+    }
+}
