488811f017db3d81cbebac23cfb6adb91c1c07cd
Rahul Tak <codebase.rahul@gmail.com>
2026-02-07 22:55:08 +0530

    switchcase

---
+++ 1. Conditional Statement/switchCase.java
@@ -0,0 +1,15 @@
+public class switchCase {
+    public static void main(String[] args) {
+        int num = 3;
+
+        switch(num){
+            case 1 : System.out.println("This is Computer lab");
+                    break;
+            case 2 : System.out.println("this is Physics lab");
+                    break;
+            case 3 : System.out.println("this is Programming lab");
+                    break;
+            default : System.out.println("this is calssroom");
+        }
+    }
+}
